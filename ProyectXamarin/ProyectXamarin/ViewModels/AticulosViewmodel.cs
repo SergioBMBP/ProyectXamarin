4d227d1748843e8336dfc951e16915f2c1199c56
sergio <sergio.gon.bar@gmail.com>
2019-05-23 10:39:21 +0200

    reset

---
+++ ProyectXamarin/ProyectXamarin/ViewModels/AticulosViewmodel.cs
@@ -0,0 +1,27 @@
+﻿using ProyectXamarin.Base;
+using ProyectXamarin.Models;
+using ProyectXamarin.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectXamarin.ViewModels
+{
+    public class AticulosViewmodel:ViewModelBase
+    {
+        IRepositoryArticulos repo;
+        public AticulosViewmodel() {
+            this.repo = new RepositoryArticulos();
+            Task.Run(async () => {
+                await CargarArticulos();
+            });
+        }
+        private void cargarArticulos()
+        {
+            this.repo.GetArticulos();
+        }
+      
+    }
+}
