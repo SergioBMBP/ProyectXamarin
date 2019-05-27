9053dab8161e8054ffedad891be2dbc60f81f691
sergiobameba <sergena23@gmail.com>
2019-05-27 13:20:27 +0200

    home terminada

--- ProyectXamarin/ProyectXamarin/Views/HomeView.xaml.cs
+++ ProyectXamarin/ProyectXamarin/Views/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ProyectXamarin.Tools;
+﻿using ProyectXamarin.Models;
+using ProyectXamarin.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,8 +19,18 @@ namespace ProyectXamarin.Views
 		{
 			InitializeComponent ();
 
+            btnmore.Clicked += Btnmore_Clicked;
             
+        }
+
+        private async void Btnmore_Clicked(object sender, EventArgs e)
+        {
+            MenuPrincipal view = new MenuPrincipal();
+            view.Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ListaArticulosView)));
+            view.IsPresented = false;
+            await App.Current.MainPage.Navigation.PushAsync(view);
+            //new NavigationPage((Page)Activator.CreateInstance(typeof(ListaArticulosView)));
 
         }
-	}
+    }
 }
